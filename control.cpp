487af19b3f3cda7be258f8ee770e3bf4d2cd23f1
micahKiiza <micahkiiza@gmail.com>
2024-09-22 00:50:18 -0700

    Add files via upload

---
+++ control.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x = 10;
+    if (x > 5) {
+        cout << "x is greater than 5" << endl;
+    } else {
+        cout << "x is less than or equal to 5" << endl;
+    }
+    return 0;
+}
